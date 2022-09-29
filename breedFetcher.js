34af3a389c50f9b6e26b0064908e221b2d5a46d2
Jeremy Buist <jbuistjbuist@gmail.com>
2022-09-29 16:10:10 -0400

    refactor and add tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,25 @@
 const request = require("request");
-const searchQuery = process.argv.slice(2, 3);
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${searchQuery}`;
 
 
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log(`Your request failed with code ${error.errno}: ${error.code}`);
-  }
-  
-  if (!error) {
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
     const data = JSON.parse(body);
-    if (data.length) return console.log(data[0].description);
-    console.log('Error: breed not found as specified please try again');
-  }
-});
+    
+    if (!data.length) {
+      callback('Cat not found!', null);
+      return;
+    }
 
+    callback(null, data[0].description);
+
+  });
+};
+
+
+module.exports = fetchBreedDescription;
